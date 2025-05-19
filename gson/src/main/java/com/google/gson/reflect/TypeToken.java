c60a695a7847757b1e405e486151779376e79421
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 19:33:26 -0700

    fix: 28 - PASS_NULLABLE - $Gson$Types.newParameterizedTypeWithOwner(null, rawType, typeArguments)); - passing @Nullable parameter 'null' where @NonNull is required

--- gson/src/main/java/com/google/gson/reflect/TypeToken.java
+++ gson/src/main/java/com/google/gson/reflect/TypeToken.java
@@ -290,6 +290,7 @@ public class TypeToken<T> {
    * {@code rawType}.
    */
   public static TypeToken<?> getParameterized(Type rawType, Type... typeArguments) {
+    Objects.requireNonNull(rawType, "rawType must not be null");
     return new TypeToken<Object>(
         $Gson$Types.newParameterizedTypeWithOwner(null, rawType, typeArguments));
   }
