a650c29e4a77008abc0e342356cc9d5772f4d305
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 19:33:48 -0700

    Revert "fix: 32 - DEREFERENCE_NULLABLE - if (stackTop.parent != null) { - dereferenced expression stackTop is @Nullable"
    This reverts commit d7a5061234cc74778fac79214035f0c08a02a92f.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -753,7 +753,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
     Node<K, V> root() {
       Node<K, V> stackTop = this.stack;
-      if (stackTop != null && stackTop.parent != null) {
+      if (stackTop.parent != null) {
         throw new IllegalStateException();
       }
       return stackTop;
