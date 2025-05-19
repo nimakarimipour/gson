67a5e55921424d112374204084269624c7c6a027
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 19:33:22 -0700

    fix: 27 - DEREFERENCE_NULLABLE - Node<K, V> pivotLeft = pivot.left; - dereferenced expression pivot is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
@@ -404,6 +404,9 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
   /** Rotates the subtree so that its root's left child is the new root. */
   private void rotateRight(Node<K, V> root) {
     Node<K, V> pivot = root.left;
+    if (pivot == null) {
+      return; // or handle the situation accordingly, e.g., throw an exception
+    }
     Node<K, V> right = root.right;
     Node<K, V> pivotLeft = pivot.left;
     Node<K, V> pivotRight = pivot.right;
