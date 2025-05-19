43b59205db7661cc366d7043476f4c683fc20468
Nima Karimipour <karimipour.nima@gmail.com>
2025-05-18 19:31:42 -0700

    Revert "fix: 7 - RETURN_NULLABLE - return TypeAdapters.JSON_ELEMENT.read(reader); - returning @Nullable expression from method with @NonNull return type"
    This reverts commit c7a4c7ac865842984be0d13e12d24d2c58a1a90e.

--- gson/src/main/java/com/google/gson/internal/Streams.java
+++ gson/src/main/java/com/google/gson/internal/Streams.java
@@ -41,15 +41,16 @@ public final class Streams {
     try {
       reader.peek();
       isEmpty = false;
-      JsonElement result = TypeAdapters.JSON_ELEMENT.read(reader);
-      if (result == null) {
-        throw new JsonParseException("Parsed result is null");
-      }
-      return result;
+      return TypeAdapters.JSON_ELEMENT.read(reader);
     } catch (EOFException e) {
+      /*
+       * For compatibility with JSON 1.5 and earlier, we return a JsonNull for
+       * empty documents instead of throwing.
+       */
       if (isEmpty) {
         return JsonNull.INSTANCE;
       }
+      // The stream ended prematurely so it is likely a syntax error.
       throw new JsonSyntaxException(e);
     } catch (MalformedJsonException e) {
       throw new JsonSyntaxException(e);
