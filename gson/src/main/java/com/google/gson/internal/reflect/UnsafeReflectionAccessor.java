5b57d448f51c0908079caeb088fb6be884e21b1e
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 13:05:33 -0700

    fix: 19 - DEREFERENCE_NULLABLE - Method method = unsafeClass.getMethod("objectFieldOffset", Field.class); - dereferenced expression unsafeClass is @Nullable

--- gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java
+++ gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java
@@ -55,19 +55,17 @@ final class UnsafeReflectionAccessor extends ReflectionAccessor {
 
   // Visible for testing only
   boolean makeAccessibleWithUnsafe(AccessibleObject ao) {
-    if (theUnsafe != null && overrideField != null) {
-      try {
+    try {
+      unsafeClass = Class.forName("sun.misc.Unsafe");
+      if (theUnsafe != null && overrideField != null && unsafeClass != null) {
         Method method = unsafeClass.getMethod("objectFieldOffset", Field.class);
-        long overrideOffset =
-            (Long) method.invoke(theUnsafe, overrideField); // long overrideOffset =
-        // theUnsafe.objectFieldOffset(overrideField);
+        long overrideOffset = (Long) method.invoke(theUnsafe, overrideField);
         Method putBooleanMethod =
             unsafeClass.getMethod("putBoolean", Object.class, long.class, boolean.class);
-        putBooleanMethod.invoke(
-            theUnsafe, ao, overrideOffset, true); // theUnsafe.putBoolean(ao, overrideOffset, true);
+        putBooleanMethod.invoke(theUnsafe, ao, overrideOffset, true);
         return true;
-      } catch (Exception ignored) { // do nothing
       }
+    } catch (Exception ignored) { // do nothing
     }
     return false;
   }
