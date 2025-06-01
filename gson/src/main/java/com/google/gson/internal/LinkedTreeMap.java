5b7651e1ecb74a529cca8b80d3481823e012b5b8
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 13:06:23 -0700

    fix: 28 - DEREFERENCE_NULLABLE - V result = created.value; - dereferenced expression created is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
@@ -98,6 +98,9 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
       throw new NullPointerException("key == null");
     }
     Node<K, V> created = find(key, true);
+    if (created == null) {
+      return null;
+    }
     V result = created.value;
     created.value = value;
     return result;
