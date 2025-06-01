7fbf62feeb46d2b519de973943ddc23458406f8a
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 13:03:03 -0700

    fix: 4 - FIELD_NO_INIT - char[] chars; - @NonNull field Streams$AppendableWriter$CurrentWrite.chars not initialized

--- gson/src/main/java/com/google/gson/internal/Streams.java
+++ gson/src/main/java/com/google/gson/internal/Streams.java
@@ -28,6 +28,7 @@ import com.google.gson.stream.MalformedJsonException;
 import java.io.EOFException;
 import java.io.IOException;
 import java.io.Writer;
+import javax.annotation.Nullable;
 
 /** Reads and writes GSON parse trees over streams. */
 public final class Streams {
@@ -98,13 +99,19 @@ public final class Streams {
 
     /** A mutable char sequence pointing at a single char[]. */
     static class CurrentWrite implements CharSequence {
-      char[] chars;
+      @Nullable char[] chars;
 
       public int length() {
+        if (chars == null) {
+          throw new NullPointerException("chars is null");
+        }
         return chars.length;
       }
 
       public char charAt(int i) {
+        if (chars == null) {
+          throw new NullPointerException("chars is null");
+        }
         return chars[i];
       }
 
