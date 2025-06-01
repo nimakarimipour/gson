37629128da71b485452726dc712992e9a25bffdb
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 13:04:55 -0700

    Revert "fix: 12 - DEREFERENCE_NULLABLE - Node<K, V> pivotLeft = pivot.left; - dereferenced expression pivot is @Nullable"
    This reverts commit c5318cf34fb9f94ede8e827a134c32acec446f76.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -708,14 +708,24 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         leavesSkipped++;
       }
 
+      /*
+       * Combine 3 nodes into subtrees whenever the size is one less than a
+       * multiple of 4. For example we combine the nodes A, B, C into a
+       * 3-element tree with B as the root.
+       *
+       * Combine two subtrees and a spare single value whenever the size is one
+       * less than a multiple of 8. For example at 8 we may combine subtrees
+       * (A B C) and (E F G) with D as the root to form ((A B C) D (E F G)).
+       *
+       * Just as we combine single nodes when size nears a multiple of 4, and
+       * 3-element trees when size nears a multiple of 8, we combine subtrees of
+       * size (N-1) whenever the total size is 2N-1 whenever N is a power of 2.
+       */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
           // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
           Node<K, V> center = right.parent;
-          if (center == null) {
-            throw new NullPointerException("Center node cannot be null here");
-          }
           Node<K, V> left = center.parent;
           center.parent = left.parent;
           stack = center;
@@ -729,9 +739,6 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
           // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
           Node<K, V> center = right.parent;
-          if (center == null) {
-            throw new NullPointerException("Center node cannot be null here");
-          }
           stack = center;
           // Construct a tree with no left child.
           center.right = right;
