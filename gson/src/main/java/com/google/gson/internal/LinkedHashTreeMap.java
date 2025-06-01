4df7e051f643feb4d7d46ba36f88b373c3ee6fb9
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 13:05:11 -0700

    Revert "fix: 14 - ASSIGN_FIELD_NULLABLE - node.next = node.prev = null; // Help the GC (for performance) - assigning @Nullable expression to @NonNull field"
    This reverts commit cc7caaac4ed0b9f6d50c1a4a869e317d681ed10c.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -112,9 +112,10 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     size = 0;
     modCount++;
 
+    // Clear all links to help GC
     Node<K, V> header = this.header;
     for (Node<K, V> e = header.next; e != header; ) {
-      Node<K, V> next = NullabilityUtil.castToNonnull(e.next, "loop condition ensures");
+      Node<K, V> next = e.next;
       e.next = e.prev = null;
       e = next;
     }
@@ -250,10 +251,8 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
    */
   void removeInternal(Node<K, V> node, boolean unlink) {
     if (unlink) {
-      if (node.next != null) {
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-      }
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
       node.next = node.prev = null; // Help the GC (for performance)
     }
 
@@ -472,7 +471,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     @Nullable Node<K, V> parent;
     @Nullable Node<K, V> left;
     @Nullable Node<K, V> right;
-    @Nullable Node<K, V> next;
+    Node<K, V> next;
     @Nullable Node<K, V> prev;
     @Nullable final K key;
     final int hash;
@@ -762,7 +761,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
   }
 
   private abstract class LinkedTreeMapIterator<T> implements Iterator<T> {
-    @Nullable Node<K, V> next = header.next;
+    Node<K, V> next = header.next;
     @Nullable Node<K, V> lastReturned = null;
     int expectedModCount = modCount;
 
