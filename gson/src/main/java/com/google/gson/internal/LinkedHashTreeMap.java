86321cd1c5a893c1edd6d3ca0c7dfebd6e82e6b4
Nima Karimipour <karimipour.nima@gmail.com>
2025-06-01 13:07:04 -0700

    fix: 32 - DEREFERENCE_NULLABLE - if (stackTop.parent != null) { - dereferenced expression stackTop is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -708,42 +708,33 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         leavesSkipped++;
       }
 
-      /*
-       * Combine 3 nodes into subtrees whenever the size is one less than a
-       * multiple of 4. For example we combine the nodes A, B, C into a
-       * 3-element tree with B as the root.
-       *
-       * Combine two subtrees and a spare single value whenever the size is one
-       * less than a multiple of 8. For example at 8 we may combine subtrees
-       * (A B C) and (E F G) with D as the root to form ((A B C) D (E F G)).
-       *
-       * Just as we combine single nodes when size nears a multiple of 4, and
-       * 3-element trees when size nears a multiple of 8, we combine subtrees of
-       * size (N-1) whenever the total size is 2N-1 whenever N is a power of 2.
-       */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
           // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
-          Node<K, V> center = right.parent;
-          Node<K, V> left = center.parent;
-          center.parent = left.parent;
-          stack = center;
-          // Construct a tree.
-          center.left = left;
-          center.right = right;
-          center.height = right.height + 1;
-          left.parent = center;
-          right.parent = center;
+          Node<K, V> center = right != null ? right.parent : null;
+          Node<K, V> left = center != null ? center.parent : null;
+          if (left != null) { // Check if left is non-null
+            center.parent = left.parent;
+            stack = center;
+            // Construct a tree.
+            center.left = left;
+            center.right = right;
+            center.height = right.height + 1;
+            left.parent = center;
+            right.parent = center;
+          }
         } else if (leavesSkipped == 1) {
           // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
-          Node<K, V> center = right.parent;
+          Node<K, V> center = right != null ? right.parent : null;
           stack = center;
           // Construct a tree with no left child.
-          center.right = right;
-          center.height = right.height + 1;
-          right.parent = center;
+          if (center != null) { // Check if center is non-null
+            center.right = right;
+            center.height = right.height + 1;
+            right.parent = center;
+          }
           leavesSkipped = 0;
         } else if (leavesSkipped == 2) {
           leavesSkipped = 0;
@@ -753,7 +744,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
     Node<K, V> root() {
       Node<K, V> stackTop = this.stack;
-      if (stackTop.parent != null) {
+      if (stackTop != null && stackTop.parent != null) {
         throw new IllegalStateException();
       }
       return stackTop;
