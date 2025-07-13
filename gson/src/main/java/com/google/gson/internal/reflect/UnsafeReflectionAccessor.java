1ef1a92bb43dc21484742d18671dce1a9e362e4a
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 07:57:18 -0700

    before:18to17 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression unsafeClass is @Nullable' /home/nima/Developer/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java:60         Method method = unsafeClass.getMethod("objectFieldOffset", Field.class);

--- gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java
+++ gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java
@@ -55,22 +55,21 @@ final class UnsafeReflectionAccessor extends ReflectionAccessor {
 
   // Visible for testing only
   boolean makeAccessibleWithUnsafe(AccessibleObject ao) {
-    if (theUnsafe != null && overrideField != null) {
-      try {
-        Method method = unsafeClass.getMethod("objectFieldOffset", Field.class);
-        long overrideOffset =
-            (Long) method.invoke(theUnsafe, overrideField); // long overrideOffset =
-        // theUnsafe.objectFieldOffset(overrideField);
-        Method putBooleanMethod =
-            unsafeClass.getMethod("putBoolean", Object.class, long.class, boolean.class);
-        putBooleanMethod.invoke(
-            theUnsafe, ao, overrideOffset, true); // theUnsafe.putBoolean(ao, overrideOffset, true);
-        return true;
-      } catch (Exception ignored) { // do nothing
+      if (theUnsafe != null && overrideField != null && unsafeClass != null) {
+        try {
+          Method method = unsafeClass.getMethod("objectFieldOffset", Field.class);
+          long overrideOffset =
+              (Long) method.invoke(theUnsafe, overrideField);
+          Method putBooleanMethod =
+              unsafeClass.getMethod("putBoolean", Object.class, long.class, boolean.class);
+          putBooleanMethod.invoke(
+              theUnsafe, ao, overrideOffset, true);
+          return true;
+        } catch (Exception ignored) {
+        }
       }
+      return false;
     }
-    return false;
-  }
 
   @Nullable
   private static Object getUnsafeInstance() {
