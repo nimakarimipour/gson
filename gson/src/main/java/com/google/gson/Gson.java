7989f34d919a97827787483100c3b1076ed258cb
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 07:56:05 -0700

    before:31to30 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression value is @Nullable' /home/nima/Developer/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/Gson.java:434         return new AtomicLong(value.longValue());

--- gson/src/main/java/com/google/gson/Gson.java
+++ gson/src/main/java/com/google/gson/Gson.java
@@ -429,10 +429,13 @@ public final class Gson {
       }
 
       @Override
-      public AtomicLong read(JsonReader in) throws IOException {
-        Number value = longAdapter.read(in);
-        return new AtomicLong(value.longValue());
-      }
+            public AtomicLong read(JsonReader in) throws IOException {
+              Number value = longAdapter.read(in);
+              if (value == null) {
+                return new AtomicLong(0);  // handle null case, assuming 0 as a default value
+              }
+              return new AtomicLong(value.longValue());
+            }
     }.nullSafe();
   }
 
