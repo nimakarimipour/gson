e6774c0153fb2dbfc8529a73b1dae66c69484f88
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 12:25:54 -0700

    Revert "fix: 16 - DEREFERENCE_NULLABLE - Node<K, V> leftLeft = left.left; - dereferenced expression left is @Nullable"
    This reverts commit 6096fcfb6d068d74fd8b7b416737b8f69be0e302.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -351,52 +351,52 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
       int delta = leftHeight - rightHeight;
       if (delta == -2) {
-        Node<K, V> rightLeft = right != null ? right.left : null;
-        Node<K, V> rightRight = right != null ? right.right : null;
+        Node<K, V> rightLeft = right.left;
+        Node<K, V> rightRight = right.right;
         int rightRightHeight = rightRight != null ? rightRight.height : 0;
         int rightLeftHeight = rightLeft != null ? rightLeft.height : 0;
 
         int rightDelta = rightLeftHeight - rightRightHeight;
         if (rightDelta == -1 || (rightDelta == 0 && !insert)) {
-          rotateLeft(node);
+          rotateLeft(node); // AVL right right
         } else {
           assert (rightDelta == 1);
-          rotateRight(right);
+          rotateRight(right); // AVL right left
           rotateLeft(node);
         }
         if (insert) {
-          break;
+          break; // no further rotations will be necessary
         }
 
       } else if (delta == 2) {
-        Node<K, V> leftLeft = left != null ? left.left : null;
-        Node<K, V> leftRight = left != null ? left.right : null;
+        Node<K, V> leftLeft = left.left;
+        Node<K, V> leftRight = left.right;
         int leftRightHeight = leftRight != null ? leftRight.height : 0;
         int leftLeftHeight = leftLeft != null ? leftLeft.height : 0;
 
         int leftDelta = leftLeftHeight - leftRightHeight;
         if (leftDelta == 1 || (leftDelta == 0 && !insert)) {
-          rotateRight(node);
+          rotateRight(node); // AVL left left
         } else {
           assert (leftDelta == -1);
-          rotateLeft(left);
+          rotateLeft(left); // AVL left right
           rotateRight(node);
         }
         if (insert) {
-          break;
+          break; // no further rotations will be necessary
         }
 
       } else if (delta == 0) {
-        node.height = leftHeight + 1;
+        node.height = leftHeight + 1; // leftHeight == rightHeight
         if (insert) {
-          break;
+          break; // the insert caused balance, so rebalancing is done!
         }
 
       } else {
         assert (delta == -1 || delta == 1);
         node.height = Math.max(leftHeight, rightHeight) + 1;
         if (!insert) {
-          break;
+          break; // the height hasn't changed, so rebalancing is done!
         }
       }
     }
