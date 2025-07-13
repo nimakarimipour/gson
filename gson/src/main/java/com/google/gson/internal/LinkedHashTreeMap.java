50996f8ed48f708e5b85bb33a238cb3dc5ab58b9
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 07:56:01 -0700

    before:32to31 - Type='DEREFERENCE_NULLABLE', message='dereferenced expression created is @Nullable' /home/nima/Developer/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java:104     V result = created.value;

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -94,17 +94,19 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     return findByObject(key) != null;
   }
 
-  @Nullable
-  @Override
-  public V put(K key, V value) {
-    if (key == null) {
-      throw new NullPointerException("key == null");
+  @Nullable @Override
+    public V put(K key, V value) {
+      if (key == null) {
+        throw new NullPointerException("key == null");
+      }
+      Node<K, V> created = find(key, true);
+      if (created == null) {
+        throw new NullPointerException("find(key, true) returned null");
+      }
+      V result = created.value;
+      created.value = value;
+      return result;
     }
-    Node<K, V> created = find(key, true);
-    V result = created.value;
-    created.value = value;
-    return result;
-  }
 
   @Override
   public void clear() {
