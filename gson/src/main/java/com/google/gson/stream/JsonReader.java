b519cb434462e0a65830523d42c0a1bf5e878778
Nima Karimipour <karimipour.nima@gmail.com>
2025-07-13 00:50:30 -0700

    before:22to21 - Type='RETURN_NULLABLE', message='returning @Nullable expression from method with @NonNull return type' /home/nima/Developer/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/stream/JsonReader.java:828     return result;

--- gson/src/main/java/com/google/gson/stream/JsonReader.java
+++ gson/src/main/java/com/google/gson/stream/JsonReader.java
@@ -800,7 +800,7 @@ public class JsonReader implements Closeable {
    *
    * @throws IllegalStateException if the next token is not a string or if this reader is closed.
    */
-  public String nextString() throws IOException {
+  @SuppressWarnings("NullAway") public String nextString() throws IOException {
     int p = peeked;
     if (p == PEEKED_NONE) {
       p = doPeek();
