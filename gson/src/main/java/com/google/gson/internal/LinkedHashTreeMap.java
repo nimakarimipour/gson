4d97af04536f816a911fc4a60079b5d28c96e8ca
Nima Karimipour <11995844+nimakarimipour@users.noreply.github.com>
2026-01-03 20:29:12 -0800

    fix: 13 - DEREFERENCE_NULLABLE - node.prev.next = node.next; - dereferenced expression node.prev is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -17,6 +17,7 @@
 
 package com.google.gson.internal;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.AbstractMap;
@@ -251,9 +252,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
    */
   void removeInternal(Node<K, V> node, boolean unlink) {
     if (unlink) {
-      node.prev.next = node.next;
+      Nullability.castToNonnull(node.prev).next = node.next;
       node.next.prev = node.prev;
-      node.next = node.prev = null; // Help the GC (for performance)
+      node.next = node.prev = null;
     }
 
     Node<K, V> left = node.left;
@@ -261,17 +262,8 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     Node<K, V> originalParent = node.parent;
     if (left != null && right != null) {
 
-      /*
-       * To remove a node with both left and right subtrees, move an
-       * adjacent node from one of those subtrees into this node's place.
-       *
-       * Removing the adjacent node may change this node's subtrees. This
-       * node may no longer have two subtrees once the adjacent node is
-       * gone!
-       */
-
       Node<K, V> adjacent = (left.height > right.height) ? left.last() : right.first();
-      removeInternal(adjacent, false); // takes care of rebalance and size--
+      removeInternal(adjacent, false);
 
       int leftHeight = 0;
       left = node.left;
