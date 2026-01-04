78921ed9b770223a7edfeb02da4af6be68880767
Nima Karimipour <11995844+nimakarimipour@users.noreply.github.com>
2026-01-03 20:30:25 -0800

    fix: 18 - ASSIGN_FIELD_NULLABLE - this.lowerBound = null; - assigning @Nullable expression to @NonNull field

--- gson/src/main/java/com/google/gson/internal/$Gson$Types.java
+++ gson/src/main/java/com/google/gson/internal/$Gson$Types.java
@@ -26,6 +26,7 @@ import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
 import java.lang.reflect.WildcardType;
 import java.util.*;
+import javax.annotation.Nullable;
 
 /**
  * Static methods for working with types.
@@ -565,7 +566,7 @@ public final class $Gson$Types {
    */
   private static final class WildcardTypeImpl implements WildcardType, Serializable {
     private final Type upperBound;
-    private final Type lowerBound;
+    @Nullable private final Type lowerBound;
 
     public WildcardTypeImpl(Type[] upperBounds, Type[] lowerBounds) {
       checkArgument(lowerBounds.length <= 1);
