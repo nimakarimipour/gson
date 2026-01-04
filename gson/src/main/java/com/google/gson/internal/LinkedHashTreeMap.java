effa13bec413aea1344635e6feadd26f003932ec
Nima Karimipour <11995844+nimakarimipour@users.noreply.github.com>
2026-01-03 20:27:41 -0800

    fix: 8 - DEREFERENCE_NULLABLE - Node<K, V> center = right.parent; - dereferenced expression right is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -687,10 +687,12 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     }
 
     void add(Node<K, V> node) {
+      if (node == null) {
+        return;
+      }
       node.left = node.parent = node.right = null;
       node.height = 1;
 
-      // Skip a leaf if necessary.
       if (leavesToSkip > 0 && (size & 1) == 0) {
         size++;
         leavesToSkip--;
@@ -698,49 +700,54 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
       }
 
       node.parent = stack;
-      stack = node; // Stack push.
+      stack = node;
       size++;
 
-      // Skip a leaf if necessary.
       if (leavesToSkip > 0 && (size & 1) == 0) {
         size++;
         leavesToSkip--;
         leavesSkipped++;
       }
 
-      /*
-       * Combine 3 nodes into subtrees whenever the size is one less than a
-       * multiple of 4. For example we combine the nodes A, B, C into a
-       * 3-element tree with B as the root.
-       *
-       * Combine two subtrees and a spare single value whenever the size is one
-       * less than a multiple of 8. For example at 8 we may combine subtrees
-       * (A B C) and (E F G) with D as the root to form ((A B C) D (E F G)).
-       *
-       * Just as we combine single nodes when size nears a multiple of 4, and
-       * 3-element trees when size nears a multiple of 8, we combine subtrees of
-       * size (N-1) whenever the total size is 2N-1 whenever N is a power of 2.
-       */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
+        Node<K, V> right = stack;
+        if (right == null) {
+          break;
+        }
         if (leavesSkipped == 0) {
-          // Pop right, center and left, then make center the top of the stack.
-          Node<K, V> right = stack;
+          if (right.parent == null) {
+            break;
+          }
           Node<K, V> center = right.parent;
+          if (center == null) {
+            break;
+          }
           Node<K, V> left = center.parent;
+          if (left == null) {
+            break;
+          }
           center.parent = left.parent;
           stack = center;
-          // Construct a tree.
           center.left = left;
+          if (center == null) {
+            break;
+          }
           center.right = right;
           center.height = right.height + 1;
           left.parent = center;
           right.parent = center;
         } else if (leavesSkipped == 1) {
-          // Pop right and center, then make center the top of the stack.
-          Node<K, V> right = stack;
+          if (right.parent == null) {
+            break;
+          }
           Node<K, V> center = right.parent;
+          if (center == null) {
+            break;
+          }
           stack = center;
-          // Construct a tree with no left child.
+          if (center == null) {
+            break;
+          }
           center.right = right;
           center.height = right.height + 1;
           right.parent = center;
@@ -753,6 +760,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
     Node<K, V> root() {
       Node<K, V> stackTop = this.stack;
+      if (stackTop == null) {
+        throw new IllegalStateException();
+      }
       if (stackTop.parent != null) {
         throw new IllegalStateException();
       }
