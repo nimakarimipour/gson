9d9f6e1bdf667ab7187a88f8fe3b7778fdff0efa
Nima Karimipour <11995844+nimakarimipour@users.noreply.github.com>
2026-01-03 20:24:22 -0800

    Revert "fix: 1 - DEREFERENCE_NULLABLE - V result = created.value; - dereferenced expression created is @Nullable"
    This reverts commit a26310f95d869d441dd6ef9de9bf78c93554eb8e.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -101,9 +101,6 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
       throw new NullPointerException("key == null");
     }
     Node<K, V> created = find(key, true);
-    if (created == null) {
-      return null;
-    }
     V result = created.value;
     created.value = value;
     return result;
