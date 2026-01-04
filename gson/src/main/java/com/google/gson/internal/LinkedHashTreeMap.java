dd839d7942cb1f8cd55cdc33cfa5836117491259
Nima Karimipour <11995844+nimakarimipour@users.noreply.github.com>
2026-01-03 20:28:56 -0800

    Revert "fix: 11 - DEREFERENCE_NULLABLE - Node<K, V> pivotLeft = pivot.left; - dereferenced expression pivot is @Nullable"
    This reverts commit 6483cffb6d8e7fe0612fe229218fd7243378ef5b.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -690,6 +690,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
       node.left = node.parent = node.right = null;
       node.height = 1;
 
+      // Skip a leaf if necessary.
       if (leavesToSkip > 0 && (size & 1) == 0) {
         size++;
         leavesToSkip--;
@@ -697,65 +698,52 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
       }
 
       node.parent = stack;
-      stack = node;
+      stack = node; // Stack push.
       size++;
 
+      // Skip a leaf if necessary.
       if (leavesToSkip > 0 && (size & 1) == 0) {
         size++;
         leavesToSkip--;
         leavesSkipped++;
       }
 
+      /*
+       * Combine 3 nodes into subtrees whenever the size is one less than a
+       * multiple of 4. For example we combine the nodes A, B, C into a
+       * 3-element tree with B as the root.
+       *
+       * Combine two subtrees and a spare single value whenever the size is one
+       * less than a multiple of 8. For example at 8 we may combine subtrees
+       * (A B C) and (E F G) with D as the root to form ((A B C) D (E F G)).
+       *
+       * Just as we combine single nodes when size nears a multiple of 4, and
+       * 3-element trees when size nears a multiple of 8, we combine subtrees of
+       * size (N-1) whenever the total size is 2N-1 whenever N is a power of 2.
+       */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
+          // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            break;
-          }
           Node<K, V> left = center.parent;
-          if (left == null) {
-            break;
-          }
-          Node<K, V> leftParent = left.parent;
-          center.parent = leftParent;
-          if (leftParent != null) {
-            if (leftParent.left == left) {
-              leftParent.left = center;
-            } else {
-              leftParent.right = center;
-            }
-          }
+          center.parent = left.parent;
           stack = center;
+          // Construct a tree.
           center.left = left;
-          if (right != null) {
-            center.right = right;
-          }
-          center.height = (right != null ? right.height : 0) + 1;
+          center.right = right;
+          center.height = right.height + 1;
           left.parent = center;
-          if (right != null) {
-            right.parent = center;
-          }
+          right.parent = center;
         } else if (leavesSkipped == 1) {
+          // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            break;
-          }
           stack = center;
-          if (right != null) {
-            center.right = right;
-          }
-          center.height = (right != null ? right.height : 0) + 1;
-          if (right != null) {
-            right.parent = center;
-          }
+          // Construct a tree with no left child.
+          center.right = right;
+          center.height = right.height + 1;
+          right.parent = center;
           leavesSkipped = 0;
         } else if (leavesSkipped == 2) {
           leavesSkipped = 0;
