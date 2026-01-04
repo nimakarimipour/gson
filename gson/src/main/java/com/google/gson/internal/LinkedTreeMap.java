9688267b03ed28e5a2c1e7055ac6891449a8a9bd
Nima Karimipour <11995844+nimakarimipour@users.noreply.github.com>
2026-01-03 20:31:19 -0800

    Revert "fix: 26 - DEREFERENCE_NULLABLE - Node<K, V> rightLeft = right.left; - dereferenced expression right is @Nullable"
    This reverts commit a73375a8a9f7e3549a684276c2942213d114c4c8.

--- gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
@@ -17,7 +17,6 @@
 
 package com.google.gson.internal;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.AbstractMap;
@@ -320,7 +319,7 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
   private void rebalance(@Nullable Node<K, V> unbalanced, boolean insert) {
     for (Node<K, V> node = unbalanced; node != null; node = node.parent) {
       Node<K, V> left = node.left;
-      Node<K, V> right = Nullability.castToNonnull(node.right);
+      Node<K, V> right = node.right;
       int leftHeight = left != null ? left.height : 0;
       int rightHeight = right != null ? right.height : 0;
 
